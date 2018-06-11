8f183d3a6c135ec54d13ab74d8cf76e266ca04c9
Koki Takahashi <hakatasiloving@gmail.com>
2018-06-11 16:05:21 +0900

    Fix ignore pattern of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
+	root: true,
 	parserOptions: {
 	  parser: 'babel-eslint',
 	},
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
 		'@hakatashi',
 		'plugin:vue/recommended',
 	],
-  // required to lint *.vue files
-  plugins: [
-    'vue',
-  ],
-  // add your custom rules here
-  rules: {
+	// required to lint *.vue files
+	plugins: [
+		'vue',
+	],
+	// add your custom rules here
+	rules: {
 		'vue/html-indent': ['error', 'tab'],
-    'node/no-unsupported-features': 'off',
+		'node/no-unsupported-features': 'off',
 	},
-  globals: {}
-}
+	globals: {},
+};
