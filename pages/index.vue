c08c53a7cf787be2d211fdfcaf57018142e0e9ab
Koki Takahashi <hakatasiloving@gmail.com>
2018-06-17 15:57:01 +0900

    Some lint

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
 			<h2 class="subtitle">
 				PWA Vue.js Application
 			</h2>
-			<div :class="['network',online ? 'online' : 'offline']">
+			<div :class="['network', online ? 'online' : 'offline']">
 				<div class="circle"/>
 				{{ online ? 'online' : 'offline' }}
 			</div>
@@ -33,12 +33,12 @@ import Logo from '~/components/Logo.vue';
 
 export default {
 	components: {Logo},
-	data () {
+	data() {
 		return {
 			online: true,
 		};
 	},
-	mounted () {
+	mounted() {
 		if (!window.navigator) {
 			this.online = false;
 			return;
@@ -47,12 +47,12 @@ export default {
 		window.addEventListener('offline', this._toggleNetworkStatus);
 		window.addEventListener('online', this._toggleNetworkStatus);
 	},
-	destroyed () {
+	destroyed() {
 		window.removeEventListener('offline', this._toggleNetworkStatus);
 		window.removeEventListener('online', this._toggleNetworkStatus);
 	},
 	methods: {
-		_toggleNetworkStatus ({type}) {
+		_toggleNetworkStatus({type}) {
 			this.online = type === 'online';
 		},
 	},
@@ -60,50 +60,50 @@ export default {
 </script>
 
 <style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+.container {
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
 
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+.title {
+	font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
+	display: block;
+	font-weight: 300;
+	font-size: 100px;
+	color: #35495e;
+	letter-spacing: 1px;
+}
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
+.subtitle {
+	font-weight: 300;
+	font-size: 42px;
+	color: #526488;
+	word-spacing: 5px;
+	padding-bottom: 15px;
+}
 
-  .links {
-    padding-top: 15px;
-  }
+.links {
+	padding-top: 15px;
+}
 
-  .network {
-    font-weight: 400;
-    font-size: 1rem;
-  }
+.network {
+	font-weight: 400;
+	font-size: 1rem;
+}
 
-  .network .circle {
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
-    background: green;
-    padding: .1rem .5rem;
-    border-radius: 1rem;
-  }
+.network .circle {
+	display: inline-block;
+	width: 1rem;
+	height: 1rem;
+	background: green;
+	padding: 0.1rem 0.5rem;
+	border-radius: 1rem;
+}
 
-  .network.offline .circle {
-    background: red;
-  }
+.network.offline .circle {
+	background: red;
+}
 </style>
