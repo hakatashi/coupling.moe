e358db073e425e6aa56bfa1cfbf9b83770f3e9a5
Koki Takahashi <hakatasiloving@gmail.com>
2018-07-15 17:27:32 +0900

    Migrate store to modules mode

--- store/index.js
+++ store/index.js
@@ -5,29 +5,28 @@ import db from '~/lib/db.js';
 
 const counterRef = db.collection('counter').doc('counter');
 
-const store = () => new Vuex.Store({
-	state: {
-		counter: {
-			value: null,
-		},
-	},
-	mutations: firebaseMutations,
-	getters: {
-		counter: (state) => state.counter.value,
-	},
-	actions: {
-		init: firebaseAction(({bindFirebaseRef}) => {
-			bindFirebaseRef('counter', counterRef);
-		}),
-		increment: firebaseAction((context) => {
-			if (context.getters.counter !== null) {
-				db.runTransaction(async(transaction) => {
-					const counterTransaction = await transaction.get(counterRef);
-					await transaction.update(counterRef, {value: counterTransaction.data().value + 1});
-				});
-			}
-		}),
+export const state = () => ({
+	counter: {
+		value: null,
 	},
 });
 
-export default store;
+export const mutations = firebaseMutations;
+
+export const getters = {
+	counter: (state) => state.counter.value,
+};
+
+export const actions = {
+	init: firebaseAction(({bindFirebaseRef}) => {
+		bindFirebaseRef('counter', counterRef);
+	}),
+	increment: firebaseAction((context) => {
+		if (context.getters.counter !== null) {
+			db.runTransaction(async(transaction) => {
+				const counterTransaction = await transaction.get(counterRef);
+				await transaction.update(counterRef, {value: counterTransaction.data().value + 1});
+			});
+		}
+	}),
+};
