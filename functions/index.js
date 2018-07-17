0a291ed88fa1c077ace0b2fa50b048496fd406a9
Koki Takahashi <hakatasiloving@gmail.com>
2018-07-17 15:07:04 +0900

    Add incrementCounter cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,17 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+firebase.initializeApp();
+
+const db = firebase.firestore();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
 	response.send('Hello from Firebase!');
+});
+
+exports.incrementCounter = functions.pubsub.topic('minute-tick').onPublish(() => {
+	const counterRef = db.collection('counter').doc('counter');
+	db.runTransaction(async (transaction) => {
+		const counterTransaction = await transaction.get(counterRef);
+		await transaction.update(counterRef, {value: counterTransaction.data().value + 1});
+	});
 });
