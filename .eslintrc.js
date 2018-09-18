6fb0770706a3e2c73deba81975867c8466c130b2
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-18 15:33:29 +0900

    Migrate eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,24 +9,11 @@ module.exports = {
 	},
 	extends: [
 		'@hakatashi',
-		'plugin:vue/recommended',
-	],
-	// required to lint *.vue files
-	plugins: [
-		'vue',
 	],
 	// add your custom rules here
 	rules: {
-		'vue/html-indent': ['error', 'tab'],
-		'vue/mustache-interpolation-spacing': ['error', 'never'],
-		'vue/max-attributes-per-line': ['error', {
-			singleline: 3,
-			multiline: {
-				max: 1,
-				allowFirstLine: false,
-			},
-		}],
 		'node/no-unsupported-features': 'off',
+		'node/no-unsupported-features/es-syntax': 'off',
 		'max-params': 'off',
 	},
 	globals: {},
